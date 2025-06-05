1c7baa96526a05a6e007e09c57ea1457cba53560
Nzanzu Lwanzo <nzanzu.lwanzo.work@gmail.com>
2025-06-05 10:14:42 +0200

    project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# PICKIMAGE
+
+`Optimize your images assets by reusing them when needed`
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./assets/css/reset.css";
+import "./assets/css/normalize.css";
+import "./assets/css/global.css";
+
+function App() {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
